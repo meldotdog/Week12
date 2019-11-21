8c01593d9178e710ec413273ad7a36974f189612
melanie dotson <jaqueline@csm-wl-dhcp-199-28.mines.edu>
2019-11-21 10:27:18 -0700

    created

---
+++ Week12-utility.py
@@ -0,0 +1,15 @@
+#
+#Melanie Dotson
+#CSCI 102 - Section D
+#Week 12 - Part B
+
+def PrintOutput(string):
+    print('OUTPUT',string)
+
+def LoadFile(file):
+    with open(file, 'r') as Afile:
+        for line in C:
+            lines=line.split()
+
+    print('OUTPUT',lines)
+
