73d3a5695d120fe38d88cea03b66858c38c55660
melanie dotson <jaqueline@csm-wl-dhcp-199-28.mines.edu>
2019-11-21 17:25:42 -0700

    final version

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,4 +1,3 @@
-#
 #Melanie Dotson
 #CSCI 102 - Section D
 #Week 12 - Part A
@@ -14,11 +13,51 @@ def LoadFile(file):
 
     return mList
 
-#this is shit
-#I am dead
-#fuck
-#yeee
-#ahhh
-#whoop
-#ughh
-#lat
+def UpdateString(string,new,loc):
+    mstring=''
+    for i in range(len(string)):
+        if i == loc:
+            mstring += new
+        else:
+            mstring += string[i]
+    print('OUTPUT',mstring)
+
+def FindWordCount(listi,name):
+    C=0
+    for i in listi:
+        i=i.split()
+        for word in i:
+            if word == name:
+                C+=1
+    return C
+            
+def ScoreFinder(list1,list2,find):
+    s=[]
+    for i in list1:
+        s.append(i.lower())
+        
+    find=find.lower()
+    if find in s:  
+        x=s.index(find)
+        print('OUTPUT',list2[x])
+    else:
+        print('OUTPUT player not found')
+    
+    
+def Union(list1,list2):
+    return list1+list2
+
+def Intersection(list1,list2):
+    mList=[]
+    for i in list1:
+        if i in list2:
+            mList.append(i)
+    return mList
+
+def NotIn(list1,list2):
+    mList=[]
+    for i in list1:
+        if i not in list2:
+            mList.append(i)
+    return mList
+
